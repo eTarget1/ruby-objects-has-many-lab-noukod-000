4d338e1783a1e4f85708935a500c2112d5b6f948
clifford <clifford@noukod.com>
2018-10-05 19:40:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,39 +1,39 @@
-# class Song
-#   attr_accessor :name, :artist
-#
-#   @@all = []
-#
-#   def initialize(name, artist=nil)
-#     @name = name
-#     @artist = artist
-#     @@all << self
-#   end
-#
-#   def artist_name
-#     if self.artist
-#       self.artist.name
-#     else
-#       nil
-#     end
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-
-
 class Song
   attr_accessor :name, :artist
 
-  def initialize(name)
+  @@all = []
+
+  def initialize(name, artist=nil)
     @name = name
+    @artist = artist
+    @@all << self
   end
 
   def artist_name
-    if @artist == nil
-      return nil
+    if self.artist
+      self.artist.name
     else
-      @artist.name
+      nil
     end
   end
-end
+
+  def self.all
+    @@all
+  end
+
+
+# class Song
+#   attr_accessor :name, :artist
+# 
+#   def initialize(name)
+#     @name = name
+#   end
+# 
+#   def artist_name
+#     if @artist == nil
+#       return nil
+#     else
+#       @artist.name
+#     end
+#   end
+# end
