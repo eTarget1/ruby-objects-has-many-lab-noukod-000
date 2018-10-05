da244a160caba454536f74fd40cf4af3514f8aa6
clifford <clifford@noukod.com>
2018-10-05 19:51:45 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,28 +1,25 @@
 class Author
-
-  attr_accessor :name, :posts
-
-  @@post_count = 0
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-    @posts = []
-  end
-
-  def add_post(post)
-    @posts << post
-    post.author = self
-    @@post_count += 1
   end
 
   def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
+    post = Post.new(title, self)
   end
 
-  def self.post_count
-    @@post_count
+  def posts
+    Post.all.select { |post| post.author == self }
   end
+  
+  def posts_by_title
+    authors_posts = []
+    Post.all_by_title.each do |post|
+      if post.author == self 
+        authors_posts << post.title 
+      end  
+    end
+    authors_posts
+  end 
 end
