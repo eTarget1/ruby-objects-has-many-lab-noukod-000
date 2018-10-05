33892650e583b4aca742b8b662f711771fbb3c24
clifford <clifford@noukod.com>
2018-10-05 20:01:05 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -15,19 +15,19 @@ class Post
 end
 
 
-# class Post 
-#   attr_accessor :title, :author 
-# 
+# class Post
+#   attr_accessor :title, :author
+#
 #   @@all = []
 #   @@all_by_title = []
-# 
+#
 #   def initialize(title, author=nil)
 #     @title = title
 #     @author = author
 #     @@all << self
 #     @@all_by_title << self
 #   end
-# 
+#
 #   def author_name
 #     if self.author
 #       self.author.name
@@ -35,12 +35,12 @@ end
 #       "Author not found!"
 #     end
 #   end
-# 
+#
 #   def self.all
 #     @@all
 #   end
-#   
+#
 #   def self.all_by_title
 #     @@all_by_title
-#   end 
+#   end
 # end
