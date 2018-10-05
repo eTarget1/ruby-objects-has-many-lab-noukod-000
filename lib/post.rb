fb64a0ee2096bfb3862c0286fb2b6b6cf18eac90
clifford <clifford@noukod.com>
2018-10-05 20:01:30 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,46 +1,46 @@
-class Post
+# class Post
+# 
+#   attr_accessor :title, :author
+#   def initialize(title)
+#     @title = title
+#   end
+# 
+#   def author_name
+#     if @author == nil
+#       return nil
+#     else
+#       @author.name
+#     end
+#   end
+# end
 
-  attr_accessor :title, :author
-  def initialize(title)
+
+class Post 
+  attr_accessor :title, :author 
+
+  @@all = []
+  @@all_by_title = []
+
+  def initialize(title, author=nil)
     @title = title
+    @author = author
+    @@all << self
+    @@all_by_title << self
   end
 
   def author_name
-    if @author == nil
-      return nil
+    if self.author
+      self.author.name
     else
-      @author.name
+      "Author not found!"
     end
   end
+
+  def self.all
+    @@all
+  end
+  
+  def self.all_by_title
+    @@all_by_title
+  end 
 end
-
-
-# class Post
-#   attr_accessor :title, :author
-#
-#   @@all = []
-#   @@all_by_title = []
-#
-#   def initialize(title, author=nil)
-#     @title = title
-#     @author = author
-#     @@all << self
-#     @@all_by_title << self
-#   end
-#
-#   def author_name
-#     if self.author
-#       self.author.name
-#     else
-#       "Author not found!"
-#     end
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-#
-#   def self.all_by_title
-#     @@all_by_title
-#   end
-# end
