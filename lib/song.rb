0579c9f77e5297ced96f82ca7d9612f836ac36f7
clifford <clifford@noukod.com>
2018-10-05 19:42:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,11 +24,11 @@ class Song
 
 # class Song
 #   attr_accessor :name, :artist
-# 
+#
 #   def initialize(name)
 #     @name = name
 #   end
-# 
+#
 #   def artist_name
 #     if @artist == nil
 #       return nil
